8faee05f45e570334adb5afd1f13c16285b24170
KINGALVI <alvialmahamud@gmail.com>
2025-05-19 01:56:26 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React_Router from './Router/React_Router'
+
+function App() {
+
+  return (
+    <>
+      <React_Router />
+    </>
+  )
+}
+
+export default App
---
+++ src/Router/React_Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
+const React_Router = () => {
+
+    const router = createBrowserRouter([
+
+        {
+            path: "/",
+            element: <h1 className="text-9xl flex justify-center">HOME PAGE</h1>
+        },
+
+    ]);
+
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
+};
+
+export default React_Router;
